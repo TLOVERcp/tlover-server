c4cc5a42caa8ae3728967aafb3c280e7b9d8ba5b
kyubeom <be_man@naver.com>
2022-04-02 20:49:53 +0900

    Modify SecretKey

--- src/main/java/com/example/tlover/global/config/jwt/Secret/SecretKey.java
+++ src/main/java/com/example/tlover/global/config/jwt/Secret/SecretKey.java
@@ -2,7 +2,7 @@ package com.example.tlover.global.config.jwt.Secret;
 
 public class SecretKey {
 
-    public static String JWT_ACCESS_SECRET_KEY = "xlavmfhwprxmzlddhkdWkdsjanglamfekhgsdfeDFHGEG";
-    public static String JWT_REFRESH_SECRET_KEY = "flvmfptlxhzms-anjffhgkfwlahfmrpTekhDFGERRGVFDSG";
+    public static String JWT_ACCESS_SECRET_KEY = "xlavmfhwprxmzlddhkdHGFDHDFWkGDSHSdsjaHFGDHDnglamfekhgsdfeDFHGEG";
+    public static String JWT_REFRESH_SECRET_KEY = "flvmfptlxhgDFSGHSDrzmsanjffhgkfHDFGwlahfmrpTekhDFGERRGVFDSG";
 
 }
