4b98ee5156efe6dcd5fec746aa0828a612e1d334
yunyc <yunyc5233@gmail.com>
2022-04-03 12:23:49 +0900

    마지막 수정

--- src/main/java/com/example/tlover/domain/user/dto/ResetPasswordResponse.java
+++ src/main/java/com/example/tlover/domain/user/dto/ResetPasswordResponse.java
@@ -1,15 +1,10 @@
 package com.example.tlover.domain.user.dto;
 
-import com.example.tlover.domain.user.entity.User;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 @Getter
 @Builder
