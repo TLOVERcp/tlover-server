e36442dccd09f37a1218cbb14ec00470bcacef45
yunyc <yunyc5233@gmail.com>
2022-04-03 12:08:02 +0900

    main과 merge 후 약간 수정

--- src/main/java/com/example/tlover/domain/user/exception/UserExceptionMessage.java
+++ src/main/java/com/example/tlover/domain/user/exception/UserExceptionMessage.java
@@ -11,8 +11,8 @@ public enum UserExceptionMessage {
     NOT_FOUND_USER_EXCEPTION_MESSAGE("해당 아이디를 찾을 수 없습니다."),
     USER_ID_DUPLICATE_EXCEPTION_MASSAGE("해당 아이디는 이미 존재하는 아이디입니다."),
     DENIED_ACCESS_EXCEPTION_MASSAGE("잘못된 접근입니다."),
-    NOT_EQUAL_PASSWORD_MASSAGE("기존의 비밀번호와 일치하지 않습니다."),
-    PASSWORD_EQUAL_EXCEPTION_MASSAGE("변경할 비밀번호와 일치합니다.");
+    NOT_EQUAL_PASSWORD_MASSAGE("입력하신 비밀번호가 기존의 비밀번호와 일치하지 않습니다."),
+    PASSWORD_EQUAL_EXCEPTION_MASSAGE("변경할 비밀번호가 기존의 비밀번호와 동일합니다.");
 
 
     private final String message;
