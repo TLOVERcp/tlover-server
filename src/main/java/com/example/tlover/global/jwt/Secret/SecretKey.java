d0207bd66a61f133a122e47239b232c387dff57b
kyubeom <be_man@naver.com>
2022-04-05 23:06:02 +0900

    JWT KeyValue Annotation

--- src/main/java/com/example/tlover/global/jwt/Secret/SecretKey.java
+++ src/main/java/com/example/tlover/global/jwt/Secret/SecretKey.java
@@ -1,9 +1,13 @@
 package com.example.tlover.global.jwt.Secret;
 
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SecretKey {
-    //어노테이션 ^^
-    public static String JWT_ACCESS_SECRET_KEY = "xlavmfhwprxmzlddhkdHGFDHDFWkGDSHSdsjaHFGDHDnglamfekhgsdfeDFHGEG";
-    public static String JWT_REFRESH_SECRET_KEY = "flvmfptlxhgDFSGHSDrzmsanjffhgkfHDFGwlahfmrpTekhDFGERRGVFDSG";
+    @Value("${jwt.access-key}")
+    public static String JWT_ACCESS_SECRET_KEY;
+
+    @Value("${jwt.refresh-key}")
+    public static String JWT_REFRESH_SECRET_KEY;
 
 }
