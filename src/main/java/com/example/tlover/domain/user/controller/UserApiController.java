d52e45aaf2a914c67342f4956db9e0d05bb4a654
kyubeom <be_man@naver.com>
2022-04-05 23:06:02 +0900

    JWT KeyValue Annotation

--- src/main/java/com/example/tlover/domain/user/controller/UserApiController.java
+++ src/main/java/com/example/tlover/domain/user/controller/UserApiController.java
@@ -3,15 +3,14 @@ package com.example.tlover.domain.user.controller;
 
 import com.example.tlover.domain.user.dto.*;
 import com.example.tlover.domain.user.entity.User;
-import com.example.tlover.domain.user.service.OAuth2UserService;
 import com.example.tlover.domain.user.exception.DeniedAccessExceptioin;
+import com.example.tlover.domain.user.service.OAuth2UserService;
 import com.example.tlover.domain.user.service.UserService;
 import com.example.tlover.global.jwt.service.JwtService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
