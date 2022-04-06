1f0e0b178288e597daac8cbddf3a61c3dbe92619
kyubeom <be_man@naver.com>
2022-04-07 02:37:51 +0900

    SMS 인증 번호 발송 API 완료

---
+++ src/main/java/com/example/tlover/global/sms/controller/SmsController.java
@@ -0,0 +1,41 @@
+package com.example.tlover.global.sms.controller;
+
+import com.example.tlover.global.sms.dto.SmsSendRequest;
+import com.example.tlover.global.sms.dto.SmsSendResponse;
+import com.example.tlover.global.sms.service.SmsService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+
+@RestController
+@RequestMapping("api/v1/sms")
+@RequiredArgsConstructor
+@Api(tags = "SMS API")
+public class SmsController {
+
+    private final SmsService smsService;
+
+    @ApiOperation(value = "문자발송", notes = "문자를 발송합니다.")
+    @PostMapping("/send")
+    public ResponseEntity<SmsSendResponse> smsSend(@Valid @RequestBody SmsSendRequest smsSendRequest) throws UnsupportedEncodingException, NoSuchAlgorithmException, URISyntaxException, InvalidKeyException, JsonProcessingException {
+
+        String value = smsService.sendSms(smsSendRequest);
+
+        return ResponseEntity.ok(SmsSendResponse.builder()
+                .value(value)
+                .build());
+    }
+
+}
