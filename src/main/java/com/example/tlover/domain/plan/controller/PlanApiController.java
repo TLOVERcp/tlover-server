9513daa0c6feebfc36983208ff88c7b67cc57c16
norangmango <rmina1030@naver.com>
2022-04-06 13:21:20 +0900

    getPlanList, getPlanDetail

--- src/main/java/com/example/tlover/domain/plan/controller/PlanApiController.java
+++ src/main/java/com/example/tlover/domain/plan/controller/PlanApiController.java
@@ -2,22 +2,21 @@ package com.example.tlover.domain.plan.controller;
 
 import com.example.tlover.domain.plan.dto.CreatePlanRequest;
 import com.example.tlover.domain.plan.dto.CreatePlanResponse;
+import com.example.tlover.domain.plan.dto.PlanDetailResponse;
+import com.example.tlover.domain.plan.dto.PlanListResponse;
 import com.example.tlover.domain.plan.entity.Plan;
 import com.example.tlover.domain.plan.service.PlanService;
-import com.example.tlover.domain.plan_region.dto.CreatePlanRegionRequest;
-import com.example.tlover.domain.plan_region.entity.PlanRegion;
 import com.example.tlover.domain.plan_region.service.PlanRegionService;
 import com.example.tlover.domain.user.controller.UserApiController;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/plans")
@@ -29,7 +28,16 @@ public class PlanApiController {
     private final PlanRegionService planRegionService;
     private final UserApiController userApiController;
 
-    @PostMapping("/createPlan")
+    /**
+     * 계획 작성 API
+     * [POST] api/v1/plans/createPlan
+     * @param createPlanRequest
+     * @param request
+     * @return ResponseEntity
+     * @author 류민아
+     */
+    @ApiOperation(value = "계획 작성", notes = "계획 작성을 합니다.")
+    @PostMapping("/create-plan")
     public ResponseEntity<CreatePlanResponse> CreatePlan(@Valid @RequestBody CreatePlanRequest createPlanRequest,
                                                          HttpServletRequest request){
         String loginId = userApiController.getLoginIdFromSession(request);
@@ -41,6 +49,44 @@ public class PlanApiController {
 
     }
 
+    /**
+     * 유저별 계획 목록 전체 조회 API
+     * [GET] api/v1/plans/plan-list
+     * 유저/상태별 계획 목록 조회 API
+     * [GET] api/v1/plans/plan-list?status=
+     * @param status
+     * @param request
+     * @return ResponseEntity
+     * @author 류민아
+     */
+    @ApiOperation(value = "계획 목록 조회", notes = "계획 목록을 조회 합니다.")
+    @GetMapping("/plan-list")
+    public ResponseEntity<List<PlanListResponse>> getPlanList(@RequestParam(required = false) String status, HttpServletRequest request) {
+        String loginId = userApiController.getLoginIdFromSession(request);
+            if (status == null) {
+                List<PlanListResponse> planListResponses = planService.getAllPlans(loginId);
+                return ResponseEntity.ok(planListResponses);
+            } else {
+                List<PlanListResponse> planListResponses = planService.getPlansByState(loginId, status);
+                return ResponseEntity.ok(planListResponses);
+            }
+    }
+
+    /**
+     * 계획내용 상세 조회 API
+     * [GET] api/v1/plans/plan-list?status=
+     * @param planTitle
+     * @param request
+     * @return ResponseEntity
+     * @author 류민아
+     */
+    @ApiOperation(value = "계획 내용 상세 조회", notes = "계획 내용을 상세 조회 합니다.")
+    @GetMapping("/plan-detail/{planTitle}")
+    public ResponseEntity<PlanDetailResponse> getPlanDetail(@PathVariable String planTitle, HttpServletRequest request) {
+        String loginId = userApiController.getLoginIdFromSession(request);
+        PlanDetailResponse planDetailResponse = planService.getPlanDetail(planTitle, loginId);
+        return ResponseEntity.ok(planDetailResponse);
+        }
+    }
 
 
-}
--- src/main/java/com/example/tlover/domain/plan/service/PlanServiceImpl.java
+++ src/main/java/com/example/tlover/domain/plan/service/PlanServiceImpl.java
@@ -1,16 +1,21 @@
 package com.example.tlover.domain.plan.service;
 
 import com.example.tlover.domain.plan.dto.CreatePlanRequest;
+import com.example.tlover.domain.plan.dto.PlanDetailResponse;
+import com.example.tlover.domain.plan.dto.PlanListResponse;
 import com.example.tlover.domain.plan.entity.Plan;
 import com.example.tlover.domain.plan.repository.PlanRepository;
 import com.example.tlover.domain.plan_region.dto.CreatePlanRegionRequest;
 import com.example.tlover.domain.plan_region.entity.PlanRegion;
+import com.example.tlover.domain.plan_region.repository.PlanRegionRepository;
 import com.example.tlover.domain.region.entity.Region;
 import com.example.tlover.domain.user.entity.User;
 import com.example.tlover.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -18,6 +23,7 @@ import org.springframework.stereotype.Service;
 public class PlanServiceImpl implements PlanService{
 
     private final PlanRepository planRepository;
+    private final PlanRegionRepository planRegionRepository;
     private final UserRepository userRepository;
 
     @Override
@@ -26,5 +32,36 @@ public class PlanServiceImpl implements PlanService{
         Plan plan = Plan.toEntity(createPlanRequest, user);
         planRepository.save(plan);
         return plan;
+    }
+
+    @Override
+    public List<PlanListResponse> getAllPlans(String loginId) {
+        User user = userRepository.findByUserLoginId(loginId).get();
+        List<Plan> plans = planRepository.findAllByUser(user);
+        List<PlanListResponse> planList = new ArrayList<>();
+        for(Plan p : plans){
+            planList.add(PlanListResponse.from(p));
+        }
+        return planList;
+    }
+
+    @Override
+    public List<PlanListResponse> getPlansByState(String loginId, String status) {
+        User user = userRepository.findByUserLoginId(loginId).get();
+        List<Plan> plans = planRepository.findAllByUserAndPlanStatus(user, status);
+        List<PlanListResponse> planList = new ArrayList<>();
+        for(Plan p : plans){
+            planList.add(PlanListResponse.from(p));
+        }
+        return planList;
+
+    }
+
+    @Override
+    public PlanDetailResponse getPlanDetail(String planTitle, String loginId) {
+        User user = userRepository.findByUserLoginId(loginId).get();
+        Plan plan = planRepository.findByUserAndPlanTitle(user, planTitle);
+        List<PlanRegion> planRegion = planRegionRepository.findAllByPlan(plan);
+        return PlanDetailResponse.from(plan, planRegion);
     }
 }
