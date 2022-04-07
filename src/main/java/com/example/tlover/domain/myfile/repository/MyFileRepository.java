13eec29ff29fb2553cb2cc7d51c7ac88186b9752
friendshipkim97 <rlawjddn5980@naver.com>
2022-04-08 00:05:22 +0900

    Delete image file, get Image, Add NotFoundMyFileException

--- src/main/java/com/example/tlover/domain/myfile/repository/MyFileRepository.java
+++ src/main/java/com/example/tlover/domain/myfile/repository/MyFileRepository.java
@@ -3,5 +3,6 @@ package com.example.tlover.domain.myfile.repository;
 import com.example.tlover.domain.myfile.entity.MyFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MyFileRepository extends JpaRepository<MyFile, Long> {
+public interface MyFileRepository extends JpaRepository<MyFile, Long>, MyFileRepositoryCustom {
+
 }
---
+++ src/main/java/com/example/tlover/domain/myfile/repository/MyFileRepositoryCustom.java
@@ -0,0 +1,9 @@
+package com.example.tlover.domain.myfile.repository;
+
+import com.example.tlover.domain.myfile.entity.MyFile;
+
+import java.util.Optional;
+
+public interface MyFileRepositoryCustom {
+    Optional<MyFile> findNotDeletedByFileId(Long fileId);
+}
