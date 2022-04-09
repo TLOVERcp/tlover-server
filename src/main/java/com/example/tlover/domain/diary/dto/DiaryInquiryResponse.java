df3509f49ff7b9515ae18d321ba725a8d2ac3e16
kyubeom <be_man@naver.com>
2022-04-10 03:07:02 +0900

    Diary inquiry API

---
+++ src/main/java/com/example/tlover/domain/diary/dto/DiaryInquiryResponse.java
@@ -0,0 +1,44 @@
+package com.example.tlover.domain.diary.dto;
+
+import com.example.tlover.domain.diary.entity.Diary;
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel(description = "다이어리 조회 위한 객체")
+public class DiaryInquiryResponse {
+
+    private Long diaryId;
+    private String diaryTitle;
+    private String  diaryPublicStatus;
+    private String diaryStatus;
+    private String diaryContext;
+    private String diaryStartDate;
+    private String diaryWriteDate;
+    private String diaryEndDate;
+    private String diaryView;
+
+    public static DiaryInquiryResponse from(Diary diary){
+        return DiaryInquiryResponse.builder()
+                .diaryId(diary.getDiaryId())
+                .diaryTitle(diary.getDiaryTitle())
+                .diaryPublicStatus(diary.getDiaryPublicStatus())
+                .diaryStatus(diary.getDiaryStatus())
+                .diaryContext(diary.getDiaryContext())
+                .diaryStartDate(diary.getDiaryStartDate())
+                .diaryWriteDate(diary.getDiaryWriteDate())
+                .diaryEndDate(diary.getDiaryEndDate())
+                .diaryView(diary.getDiaryView())
+                .build();
+    }
+
+
+}
