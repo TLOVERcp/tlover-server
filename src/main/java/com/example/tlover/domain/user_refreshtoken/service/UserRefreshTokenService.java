4cb5d2360ff0cbe9cd82719860bd4484b0d7088e
kyubeom <be_man@naver.com>
2022-04-21 02:54:09 +0900

    세션 로그인 대체 JWT 로그인 정보

--- src/main/java/com/example/tlover/domain/user_refreshtoken/service/UserRefreshTokenService.java
+++ src/main/java/com/example/tlover/domain/user_refreshtoken/service/UserRefreshTokenService.java
@@ -3,6 +3,7 @@ package com.example.tlover.domain.user_refreshtoken.service;
 import com.example.tlover.domain.user.entity.User;
 import com.example.tlover.domain.user_refreshtoken.entity.UserRefreshToken;
 
+
 public interface UserRefreshTokenService {
 
     long insertRefreshToken(String refreshJwt, User user);
