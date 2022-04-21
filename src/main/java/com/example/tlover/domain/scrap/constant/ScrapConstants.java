486bf03687abb1340eed2a0351e162f7315a415c
friendshipkim97 <rlawjddn5980@naver.com>
2022-04-22 02:30:34 +0900

    다이어리 스크랩 랭킹순, 페이지별로 가져오기 추가, 해당 유저의 해당 다이어리 스크랩 여부 확인하기 추가

--- src/main/java/com/example/tlover/domain/scrap/constant/ScrapConstants.java
+++ src/main/java/com/example/tlover/domain/scrap/constant/ScrapConstants.java
@@ -1,6 +1,5 @@
 package com.example.tlover.domain.scrap.constant;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -8,9 +7,11 @@ public class ScrapConstants {
     @Getter
     @RequiredArgsConstructor
     public enum EScrapResponseMessage{
-        eScrapCountSuccess("스크랩 수를 조회했습니다."),
+        eScrapCountSuccessMessage("스크랩 수를 조회했습니다."),
         eCreateScrapMessage("스크랩을 완료했습니다."),
-        eDeleteScrapMessage("스크랩을 취소했습니다.");
+        eDeleteScrapMessage("스크랩을 취소했습니다."),
+        eScrapOrNotMessage("스크랩 여부를 확인했습니다."),
+        eGetDiariesByScrapRankingMessage("모든 다이어리를 스크랩 랭킹 순으로 조회합니다.");
         private final String message;
     }
 
--- src/main/java/com/example/tlover/domain/scrap/controller/ScrapController.java
+++ src/main/java/com/example/tlover/domain/scrap/controller/ScrapController.java
@@ -1,20 +1,23 @@
 package com.example.tlover.domain.scrap.controller;
 
+import com.example.tlover.domain.diary.dto.DiaryInquiryResponse;
 import com.example.tlover.domain.scrap.constant.ScrapConstants.EScrapResponseMessage;
-import com.example.tlover.domain.scrap.dto.ScrapChangeRequest;
-import com.example.tlover.domain.scrap.dto.ScrapChangeResponse;
-import com.example.tlover.domain.scrap.dto.ScrapCountResponse;
+import com.example.tlover.domain.scrap.dto.*;
 import com.example.tlover.domain.scrap.service.ScrapService;
+import com.example.tlover.global.dto.PaginationDto;
 import com.example.tlover.global.dto.ResponseDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/scraps")
@@ -24,13 +27,25 @@ public class ScrapController {
 
     private final ScrapService scrapService;
 
+    /**
+     * 다이어리의 스크랩 수 조회
+     * @param diaryId
+     * @return ResponseDto<ScrapCountResponse>
+     * @author 김정우
+     */
     @ApiOperation(value = "다이어리의 스크랩 수 조회", notes = "다이어리의 스크랩 수를 조회합니다.")
     @GetMapping("{diaryId}")
     public ResponseEntity<ResponseDto<ScrapCountResponse>> getScrapCount(@PathVariable Long diaryId) {
-        return ResponseEntity.ok(ResponseDto.create(EScrapResponseMessage.eScrapCountSuccess.getMessage()
+        return ResponseEntity.ok(ResponseDto.create(EScrapResponseMessage.eScrapCountSuccessMessage.getMessage()
                 , this.scrapService.getScrapCount(diaryId)));
     }
 
+    /**
+     * 다이어리 스크랩 생성/삭제
+     * @param scrapChangeRequest
+     * @return ResponseDto<ScrapCountResponse>
+     * @author 김정우
+     */
     @ApiOperation(value = "다이어리 스크랩 생성/삭제", notes = "다이어리의 스크랩을 생성/삭제 합니다.")
     @PostMapping
     public ResponseEntity<ResponseDto<ScrapChangeResponse>> changeScrap(@Valid @RequestBody ScrapChangeRequest scrapChangeRequest) {
@@ -44,5 +59,34 @@ public class ScrapController {
         }
         else return ResponseEntity.noContent().build();
     }
+
+    /**
+     * 해당 유저의 해당 다이어리 스크랩 여부 조회
+     * @param scrapOrNotRequest
+     * @return ResponseDto<ScrapOrNotResponse>
+     * @author 김정우
+     */
+    @ApiOperation(value = "해당 유저의 해당 다이어리 스크랩 여부 조회", notes = "해당 유저의 해당 다이어리 스크랩 여부를 조회합니다.")
+    @GetMapping("/whether")
+    public ResponseEntity<ResponseDto<ScrapOrNotResponse>> getScrapOrNot(@Valid @RequestBody ScrapOrNotRequest scrapOrNotRequest) {
+        return ResponseEntity.ok(ResponseDto.create(EScrapResponseMessage.eScrapOrNotMessage.getMessage()
+                , this.scrapService.getScrapOrNot(scrapOrNotRequest)));
+    }
+
+    /**
+     * 모든 다이어리 스크랩 많은 순 조회
+     * @param pageable
+     * @return ResponseDto<PaginationDto<List<DiaryInquiryByScrapRankingResponse>>>
+     * @author 김정우
+     */
+
+    @ApiOperation(value = "모든 다이어리 스크랩 많은 순 조회", notes = "모든 다이어리를 스크랩 많은 순으로 조회합니다.")
+    @GetMapping("/ranking")
+    public ResponseEntity<ResponseDto<PaginationDto<List<DiaryInquiryByScrapRankingResponse>>>> getDiariesByScrapRanking(@PageableDefault Pageable pageable) {
+        return ResponseEntity.ok(ResponseDto.create(EScrapResponseMessage.eGetDiariesByScrapRankingMessage.getMessage()
+                , this.scrapService.getDiariesByScrapRanking(pageable)));
+    }
+
+
 
 }
