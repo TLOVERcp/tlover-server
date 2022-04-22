755be717fdf2e8ecde094332e8e2d7a6f31bfe5e
MAVOTUNA <sdm03131229@gmail.com>
2022-04-22 22:56:45 +0900

    다이어리 등록 추가 삭제 , 계획 공유 및 수락 , 예외처리

--- src/main/java/com/example/tlover/domain/authority_diary/service/AuthorityDiaryServiceImpl.java
+++ src/main/java/com/example/tlover/domain/authority_diary/service/AuthorityDiaryServiceImpl.java
@@ -3,7 +3,6 @@ package com.example.tlover.domain.authority_diary.service;
 import com.example.tlover.domain.authority_diary.entity.AuthorityDiary;
 import com.example.tlover.domain.authority_diary.repository.AuthorityDiaryRepository;
 import com.example.tlover.domain.authority_plan.dto.SharePlanRequest;
-import com.example.tlover.domain.authority_plan.entity.AuthorityPlan;
 import com.example.tlover.domain.diary.entity.Diary;
 import com.example.tlover.domain.diary.repository.DiaryRepository;
 import com.example.tlover.domain.user.entity.User;
@@ -11,6 +10,9 @@ import com.example.tlover.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -31,10 +33,46 @@ public class AuthorityDiaryServiceImpl implements AuthorityDiaryService{
 
     @Override
     public void sharePlan(Long diaryId, SharePlanRequest sharePlanRequest) {
-        User user = userRepository.findByUserNickName(sharePlanRequest.getUserNickName()).get();
-        Diary diary = diaryRepository.findByDiaryId(diaryId);
-        AuthorityDiary authorityDiary = AuthorityDiary.toEntity(user, diary, "REQUEST");
-        authorityDiaryRepository.save(authorityDiary);
+        
+            Optional<User> optionalUser= userRepository.findByUserNickName(sharePlanRequest.getUserNickName());
+
+            if(optionalUser.isEmpty()) throw new RuntimeException("잘못된 유저입니다.");
+
+            User user = optionalUser.get();
+
+        Optional<AuthorityDiary> cadr = authorityDiaryRepository.findByUserUserId(user.getUserId());
+
+//        비어있어야 정상임.
+        if(cadr.isEmpty()) {
+
+            Diary diary = diaryRepository.findByDiaryId(diaryId);
+            AuthorityDiary authorityDiary = AuthorityDiary.toEntity(user, diary, "REQUEST");
+            authorityDiaryRepository.save(authorityDiary);
+        } else{
+            AuthorityDiary authorityDiary = cadr.get();
+            if(authorityDiary.getAuthorityDiaryStatus().equals("REQUEST")) throw new RuntimeException("이미 권한 요청을 한 유저입니다.");
+            if(authorityDiary.getAuthorityDiaryStatus().equals("REJECT")) throw new RuntimeException("요청을 거절한 유저입니다.");
+            else if(authorityDiary.getAuthorityDiaryStatus().equals("HOST")) throw new RuntimeException("이미 호스트 입니다.");
+        }
     }
+
+    @Override
+    @Transactional
+    public void updateAcceptAuthorityDiary(Long authorityDiaryId) {
+
+        Optional<AuthorityDiary> cadr = authorityDiaryRepository.findByAuthorityDiaryId(authorityDiaryId);
+
+        if(cadr.isEmpty()) throw new RuntimeException("존재하지 않는 게시물 입니다.");
+
+        cadr.get().setAuthorityDiaryStatus("ACCEPT");
+
+    }
+
+    @Override
+    public void updateRejectAuthorityDiary(Long authorityDiaryId) {
+//        AuthorityDiary authorityDiary = authorityDiaryRepository.findByAuthorityDiaryId(authorityDiaryId);
+//        authorityDiary.setAuthorityDiaryStatus("REJECT");
+    }
+
 
 }
